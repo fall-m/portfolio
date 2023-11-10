e5137cc77c6b0cf0e82705f8a974c6acf0de37a6
MFall03 <145122757+MFall03@users.noreply.github.com>
2023-11-10 15:50:35 +0100

    Add files via upload

--- js/tp9js/tp9.js
+++ js/tp9js/tp9.js
@@ -36,7 +36,55 @@ function affichagetab2(){
     var n1 = prompt("Définir le nombre de ligne du tableau")
     document.write("<table border=2px width=30%")
     for(let i=1;i<=n1;i++){
-        document.write("<tr><td>*</td><td>*</td><tr>")
+        document.write("<tr><td>"+i+"</td><td>*</td><tr>")
     }
     document.write("</table>")
+}
+
+function swich(){
+    var color = prompt("entrez une couleur")
+    switch(color){
+        case "rouge" : document.body.style.background="red";
+        break;
+        case "ROUGE" : document.body.style.background="red";
+        break;
+        case "bleu" : document.body.style.background="blue";
+        break;
+        case "BLEU" : document.body.style.background="blue";
+        break;
+        case "vert" : document.body.style.background="green";
+        break;
+        case "VERT" : document.body.style.background="green";
+        break;
+        default:alert("couleur non définie")
+
+    }
+}
+
+function cdc(){
+    var chaine = prompt("Donnez un mot");
+    document.write(chaine.toUpperCase()+"<br>");
+    document.write(chaine.toLowerCase()+"<br>");
+    document.write("la chaine contient "+chaine.length+" caractères "+"<br>");
+    document.write("la première lettre est "+chaine.substr(0,1)+"<br>");
+}
+
+function seconnecter3(){
+    
+    var i = 0;
+
+    do {
+        var login = prompt("Donnez votre nom d'utilisateur");
+        var password = prompt("Entrez votre mot de passe");
+        if(login=="admin" && password =="admin")
+        {
+            document.write("Bienvenue : "+login);
+            break;
+        }
+        else
+            alert("accès refusé");
+            i++;
+    } while (i < 3);
+    if(i==3)
+    alert("nombre de tentative expiré");
 }
